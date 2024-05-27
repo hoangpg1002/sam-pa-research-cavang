12f4762faa2b8f27483d6695058eed8f4ed0e7c8
Huy Hoàng Nguyễn <cavangmauxanh@gmail.com>
2024-05-27 21:44:24 +0700

    Update train.py

--- train.py
+++ train.py
@@ -61,7 +61,7 @@ def main(net, train_datasets, valid_datasets, ):
     lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 10)
     lr_scheduler.last_epoch = 0
     #train(net,optimizer, train_dataloaders, valid_dataloaders, lr_scheduler)
-    sam = sam_model_registry["vit_b"](checkpoint=r"D:\StableDiffusion\sam-hq\train\pretrained_checkpoint\sam_vit_b_01ec64.pth").to(device="cuda")
+    sam = sam_model_registry["vit_b"](checkpoint="/kaggle/working/training/pretrained_checkpoint/sam_vit_b_01ec64.pth").to(device="cuda")
     evaluate(net, sam, valid_dataloaders)
 
 def train(net, optimizer, train_dataloaders, valid_dataloaders, lr_scheduler):
@@ -74,7 +74,7 @@ def train(net, optimizer, train_dataloaders, valid_dataloaders, lr_scheduler):
     net.train()
     _ = net.to(device="cuda")
     
-    sam = sam_model_registry["vit_b"](checkpoint=r"D:\StableDiffusion\sam-hq\train\pretrained_checkpoint\sam_vit_b_01ec64.pth")
+    sam = sam_model_registry["vit_b"](checkpoint="/kaggle/working/training/pretrained_checkpoint/sam_vit_b_01ec64.pth")
     _ = sam.to(device="cuda")
     #sam = torch.nn.parallel.DistributedDataParallel(sam, device_ids=[args.gpu], find_unused_parameters=args.find_unused_params)
     
