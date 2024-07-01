78d7e745f3b05f6822cec955eebfdff03269904c
Huy Hoàng Nguyễn <cavangmauxanh@gmail.com>
2024-07-01 21:01:05 +0700

    Update zero_shot_ins_eval.py

--- eval/zero_shot_ins_eval.py
+++ eval/zero_shot_ins_eval.py
@@ -59,6 +59,7 @@ def eval_zero_shot(eval_type,val_img_path,val_json_path,vit_det_file_path,sam_ch
             point_coords=None,
             point_labels=None,
             box=input_box[None, :],
+            # hq_token_only=True,
             multimask_output = False,
         )
         
