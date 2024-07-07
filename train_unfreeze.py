0879f3693de995469a16d7f9cf5855982d3cca7d
Huy Hoàng Nguyễn <cavangmauxanh@gmail.com>
2024-07-07 09:16:55 +0700

    Update train_unfreeze.py

--- train_unfreeze.py
+++ train_unfreeze.py
@@ -60,9 +60,9 @@ def main(net, train_datasets, valid_datasets, ):
     optimizer = optim.Adam(net.parameters(), lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0)
     lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 10)
     lr_scheduler.last_epoch = 3
-    train(net,optimizer, train_dataloaders, valid_dataloaders, lr_scheduler)
+    #train(net,optimizer, train_dataloaders, valid_dataloaders, lr_scheduler)
     sam = sam_model_registry["vit_b"](checkpoint="/kaggle/working/training/pretrained_checkpoint/sam_vit_b_01ec64.pth").to(device="cuda")
-    #evaluate(net, sam, valid_dataloaders)
+    evaluate(net, sam, valid_dataloaders)
 
 def train(net, optimizer, train_dataloaders, valid_dataloaders, lr_scheduler):
     if misc.is_main_process():
