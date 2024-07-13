54fd14376c5226b5267bfa0300ddfe67d5c112f7
Huy Hoàng Nguyễn <cavangmauxanh@gmail.com>
2024-07-13 08:15:19 +0700

    Update train_unfreeze.py

--- train_unfreeze.py
+++ train_unfreeze.py
@@ -57,7 +57,7 @@ def main(net, train_datasets, valid_datasets, ):
  
     ### --- Step 3: Train or Evaluate ---
     print("--- define optimizer ---")
-    optimizer = optim.Adam(net.parameters(), lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0)
+    optimizer = optim.Adam(net.parameters(), lr=0.0001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0)
     lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 10)
     lr_scheduler.last_epoch = 0
     train(net,optimizer, train_dataloaders, valid_dataloaders, lr_scheduler)
