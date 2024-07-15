1432a1612bcac6fcfbcaf82003b00a3f923bd74a
Huy Hoàng Nguyễn <cavangmauxanh@gmail.com>
2024-07-15 18:44:07 +0700

    cavang

--- model/mask_decoder_pa_unfreeze.py
+++ model/mask_decoder_pa_unfreeze.py
@@ -218,7 +218,7 @@ class MaskDecoderPA(MaskDecoder):
         #-----------------------prompt adapter-------------------------------
         self.prompt_adapter = PromptAdapater(transformer_dim,self.transformer.num_heads)
         if is_train==True:
-            self.load_state_dict(torch.load("/kaggle/working/training/pretrained_checkpoint/epoch_13.pth"))
+            self.load_state_dict(torch.load("/kaggle/working/training/pretrained_checkpoint/epoch_11.pth"))
             print("load success!")
 
 
